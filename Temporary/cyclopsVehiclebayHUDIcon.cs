58e29471a09e0ac475bb9c6c2e0fe5987096e0c8
Slothy-lol <76905744+Slothy-lol@users.noreply.github.com>
2022-02-21 21:50:03 +0000

    Create cyclopsVehiclebayHUDIcon.cs

---
+++ Temporary/cyclopsVehiclebayHUDIcon.cs
@@ -0,0 +1,89 @@
+using MoreCyclopsUpgrades.API;
+using MoreCyclopsUpgrades.API.StatusIcons;
+using QModManager.API.ModLoading;
+using System.Reflection;
+using Sprite = Atlas.Sprite;
+using UnityEngine;
+using SMLHelper.V2.Utility;
+using System.IO;
+
+namespace cyclopsVehiclebayHUDIcon
+{
+    internal class MySubStatus : CyclopsStatusIcon
+    {
+        public MySubStatus(SubRoot cyclops) : base(cyclops)
+        {
+
+        }
+        private VehicleDockingBay dockingBay;
+        private VehicleDockingBay DockingBay => dockingBay ?? (dockingBay = Cyclops.GetComponentInChildren<VehicleDockingBay>());
+
+        private SeaMoth seaMoth;
+        private Exosuit exosuit;
+
+        private SeaMoth DockedSeamoth => this.DockingBay?.dockedVehicle as SeaMoth;
+        private Exosuit DockedExosuit => this.DockingBay?.dockedVehicle as Exosuit;
+
+        public bool SeamothInBay
+        {
+            get
+            {
+                seaMoth = this.DockedSeamoth;
+                return seaMoth != null;
+            }
+        }
+
+        public bool ExosuitInBay
+        {
+            get
+            {
+                exosuit = this.DockedExosuit;
+                return exosuit != null;
+            }
+        }
+
+
+        public override bool ShowStatusIcon => ExosuitInBay || SeamothInBay;
+
+
+        public override Sprite StatusSprite()
+        {
+            if (ExosuitInBay == true)
+            {
+                return ImageUtils.LoadSpriteFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets", "ExosuitIcon.png"));
+            }
+            if (SeamothInBay == true)
+            {
+                return ImageUtils.LoadSpriteFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets", "SeamothIcon.png"));
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        public override string StatusText()
+        {
+            string statusText = "Vehicle Docked.";
+            return statusText;
+        }
+
+        public override Color StatusTextColor()
+        {
+            return Color.white;
+        }
+    }
+
+    // Your main patching class must have the QModCore attribute (and must be public)
+    [QModCore]
+    public static class MyInitializerClass
+    {
+        // Your patching method must have the QModPatch attribute (and must be public)
+        [QModPatch]
+        public static void MyInitializationMethod()
+        {
+            MCUServices.Register.CyclopsStatusIcon<MySubStatus>((SubRoot cyclops) => new MySubStatus(cyclops));
+        }
+    }
+}
