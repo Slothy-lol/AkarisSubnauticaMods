a6672fb303b0320e2167c7f09c84fe80cf652d9f
Slothy <snorlaxlover10@gmail.com>
2022-03-09 21:37:54 +0000

    h

---
+++ MoreEngineEfficiencyModulesBZ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MoreEngineEfficiencyModulesBZ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoreEngineEfficiencyModulesBZ")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("777d7953-f1c3-444a-bc6c-f63e3012786b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MoreEngineEfficiencyModulesBZ/README.md
@@ -0,0 +1,19 @@
+## More Engine Efficiency Modules BZ
+
+This mod re-adds the Engine Efficiency Module and adds a Mark 2 and Mark 3 Engine Efficiency Module for the Seatruck and PRAWN Suit.<br>
+This is a port of More Engine Efficiency Modules for SN1, with slight changes so it works properly.
+    
+    MK1 ID: VehiclePowerUpgradeModule
+
+    MK2 ID: VehiclePowerUpgradeModuleMK2
+    
+    MK3 ID: VehiclePowerUpgradeModuleMK3
+
+    The original is crafted at the Vehicle Upgrade Console in a Moonpool.
+    The MK2 and MK3 are crafted at a modification station after unlocking the vehicle upgrade console.
+    
+    Needs: SMLHelper, QModManager.
+    
+    ThunderStore Page: https://subnautica.thunderstore.io/package/Akari/More_Engine_Efficiency_Modules_BZ/
+    
+    To use with Upgraded Vehicles, please go to the Subnautica Modding Server and ping me: "Akari - アカリ#1302" for more information.
