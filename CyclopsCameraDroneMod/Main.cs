c2fd23ba84467f37dabae4ab7ba1911dcbbd6b5a
Nagorogan <97289845+Nagorogan@users.noreply.github.com>
2022-03-20 20:22:56 -0500

    Update Main.cs

--- CyclopsCameraDroneMod/Main.cs
+++ CyclopsCameraDroneMod/Main.cs
@@ -1,7 +1,19 @@
-﻿using HarmonyLib;
+using HarmonyLib;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Options;
+using SMLHelper.V2.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger = QModManager.Utility.Logger;
 using UnityEngine;
 using UWE;
 using System.Collections;
+using CyclopsCameraDroneMod.QMods;
 
 namespace CyclopsCameraDroneMod.Main
 {
@@ -16,28 +28,42 @@ namespace CyclopsCameraDroneMod.Main
             [HarmonyPostfix]
             public static void HandleInputPatch(CyclopsExternalCams __instance, ref bool __result)
             {
-                if(Input.GetKeyUp(KeyCode.P))
+                /*if (__instance.GetUsingCameras())
+                {
+                    Logger.Log(Logger.Level.Info, "Not using cameras");
+                    return;
+                }*/
+                KeyCode droneButton = QMod.Config.droneKey;
+                if(Input.GetKeyUp(droneButton))
                 {
                     __instance.ExitCamera();
 
-                    CoroutineHost.StartCoroutine(CreateAndControl(__instance));
+                    CoroutineHost.StartCoroutine(createAndControl(__instance));
 
                     __result = true;
                 }
             }
-            private static IEnumerator CreateAndControl(CyclopsExternalCams __instance)
+            private static IEnumerator createAndControl(CyclopsExternalCams __instance)
             {
                 var coroutineTask = CraftData.GetPrefabForTechTypeAsync(TechType.MapRoomCamera, false);
                 yield return coroutineTask;
                 var prefab = coroutineTask.GetResult();
 
+                coroutineTask = CraftData.GetPrefabForTechTypeAsync(TechType.Battery, false);
+                yield return coroutineTask;
+                var batteryPrefab = coroutineTask.GetResult();
+
                 Vector3 position = GetSpawnPosition(__instance.transform.parent.gameObject, Player.main.gameObject);
                 MapRoomCamera cyclopsCameraDrone = GameObject.Instantiate(prefab, position, Player.main.transform.rotation).GetComponent<MapRoomCamera>();
                 cyclopsCameraDrone.gameObject.name = CameraName;
-                
+
+                cyclopsCameraDrone.energyMixin.battery = GameObject.Instantiate(batteryPrefab).GetComponent<Battery>();
+
                 yield return new WaitUntil(() => cyclopsCameraDrone.inputStackDummy != null);
-                cyclopsCameraDrone.energyMixin.energy = 100f;
+                if (Player.main.currChair != null) { Player.main.ExitPilotingMode(); }
                 cyclopsCameraDrone.ControlCamera(Player.main, null);
+                Player.main.ExitLockedMode(false, false);
+                Player.main.EnterLockedMode(null);
             }
             static Vector3 GetSpawnPosition(GameObject cyclopsObject, GameObject playerObject)
             {
@@ -55,7 +81,7 @@ namespace CyclopsCameraDroneMod.Main
                 combinedMatrix.SetRow(2, row3);
                 combinedMatrix.SetRow(3, row4);
 
-                return combinedMatrix.MultiplyPoint3x4(new Vector3(0, -12, 0));
+                return combinedMatrix.MultiplyPoint3x4(new Vector3(0, -12, 0)); // set the position relative to the Cyclops' Rotation and scale and the Player's Position
             }
 
             [HarmonyPatch(typeof(MapRoomCamera), nameof(MapRoomCamera.FreeCamera))]
@@ -67,7 +93,8 @@ namespace CyclopsCameraDroneMod.Main
                     return true;
                 }
                 InputHandlerStack.main.Pop(__instance.inputStackDummy);
-                __instance.controllingPlayer.ExitLockedMode(false, false);
+                Player player = __instance.controllingPlayer;
+                player.ExitLockedMode(false, false);
                 __instance.controllingPlayer = null;
                 if (resetPlayerPosition)
                 {
@@ -87,8 +114,6 @@ namespace CyclopsCameraDroneMod.Main
                 Player.main.SetHeadVisible(false);
                 __instance.lightsParent.SetActive(false);
 
-                if (Player.main.currChair != null) { Player.main.ExitPilotingMode(); }
-                
                 if(__instance.name == CameraName)
                 {
                     GameObject.Destroy(__instance.gameObject);
@@ -99,9 +124,19 @@ namespace CyclopsCameraDroneMod.Main
             [HarmonyPostfix]
             public static void fixDistance(MapRoomCamera __instance, ref float __result)
             {
-                if(__instance.name == CameraName/* && !QMod.Config.InfiniteDistance*/) //make config for infinite distance eventually
+                if(__instance.name == CameraName/* && !QMods.Config.InfiniteDistance*/) //make config for infinite distance eventually
                 {
-                    __result = (Player.main.transform.position - __instance.transform.position).magnitude/* - QMod.Config.ExtraDistance*/; //make config for extra distance eventually
+                    __result = (Player.main.transform.position - __instance.transform.position).magnitude/* - QMods.Config.ExtraDistance*/; //make config for extra distance eventually
+                }
+            }
+            [HarmonyPatch(typeof(MapRoomCamera), nameof(MapRoomCamera.StabilizeRoll))]
+            [HarmonyPostfix]
+            public static void FixPlayerMovement(MapRoomCamera __instance)
+            {
+                if (__instance.name == CameraName)
+                {
+                    Player.main.ExitLockedMode(false, false);
+                    Player.main.EnterLockedMode(null);
                 }
             }
         }
