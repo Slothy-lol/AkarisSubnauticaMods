d3d3e18db8cb82de4ce9b25f20e220f084606c2b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-03-26 19:50:23 -0400

    Helpers class w/ GetFmodAsset

---
+++ CyclopsCameraDroneMod/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CyclopsCameraDroneMod
+{
+    public static class Helpers
+    {
+        public static FMODAsset GetFmodAsset(string audioPath)
+        {
+            FMODAsset asset = ScriptableObject.CreateInstance<FMODAsset>();
+            asset.path = audioPath;
+            return asset;
+        }
+    }
+}
