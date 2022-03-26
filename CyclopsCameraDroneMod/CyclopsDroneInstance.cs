706bc3de9415007132e8960513543adc502199f6
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-03-26 19:55:06 -0400

    more efficient way to add emitters

--- CyclopsCameraDroneMod/CyclopsDroneInstance.cs
+++ CyclopsCameraDroneMod/CyclopsDroneInstance.cs
@@ -12,21 +12,26 @@ namespace CyclopsCameraDroneMod
         private static FMODAsset deployBeaconSound = Helpers.GetFmodAsset("event:/sub/cyclops/load_decoy");
         private static FMODAsset drillLoopSound = Helpers.GetFmodAsset("event:/tools/gravsphere/loop_actual");
         private static FMODAsset mineEmitterLoopSound = Helpers.GetFmodAsset("event:/sub/exo/drill_hit_loop");
+        private static FMODAsset tractorBeamLoopSound = Helpers.GetFmodAsset("event:/sub/rocket/call_lift_loop");
 
         private FMOD_CustomLoopingEmitter drillEmitter;
         private FMOD_CustomLoopingEmitter mineEmitter;
+        private FMOD_CustomLoopingEmitter tractorBeamEmitter;
 
         private void Start()
         {
-            drillEmitter = gameObject.AddComponent<FMOD_CustomLoopingEmitter>();
-            drillEmitter.SetAsset(drillLoopSound);
-            drillEmitter.followParent = true;
-            drillEmitter.restartOnPlay = false;
+            drillEmitter = AddLoopingEmitter(drillLoopSound);
+            mineEmitter = AddLoopingEmitter(mineEmitterLoopSound);
+            tractorBeamEmitter = AddLoopingEmitter(tractorBeamLoopSound);
+        }
 
-            mineEmitter = gameObject.AddComponent<FMOD_CustomLoopingEmitter>();
-            mineEmitter.SetAsset(mineEmitterLoopSound);
-            mineEmitter.followParent = true;
-            mineEmitter.restartOnPlay = false;
+        private FMOD_CustomLoopingEmitter AddLoopingEmitter(FMODAsset asset)
+        {
+            var emitter = gameObject.AddComponent<FMOD_CustomLoopingEmitter>();
+            emitter.SetAsset(asset);
+            emitter.followParent = true;
+            emitter.restartOnPlay = false;
+            return emitter;
         }
 
         public void StartDrillSound()
@@ -47,6 +52,16 @@ namespace CyclopsCameraDroneMod
         public void StopMineSound()
         {
             mineEmitter.Stop();
+        }
+
+        public void StartTractorBeamSound()
+        {
+            tractorBeamEmitter.Play();
+        }
+
+        public void StopTractorBeamSound()
+        {
+            tractorBeamEmitter.Stop();
         }
 
         public void PlayBeaconSound()
