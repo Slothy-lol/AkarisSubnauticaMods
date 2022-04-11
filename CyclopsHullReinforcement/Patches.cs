c8d68d84c1f530952dd845f6d1570558b6afaf03
Slothy-lol <snorlaxlover10@gmail.com>
2022-04-11 15:08:38 +0100

    Stuff for Hull reinforcement mod

--- CyclopsHullReinforcement/Patches.cs
+++ CyclopsHullReinforcement/Patches.cs
@@ -4,8 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
+using UnityEngine;
+using MoreCyclopsUpgrades.API;
 
 namespace CyclopsHullReinforcement.Patches
 {
-
+    [HarmonyPatch(typeof(DamageSystem), nameof(DamageSystem.CalculateDamage))]
+    class Postfix
+    {
+        [HarmonyPostfix]
+        public static void DamagePatch(GameObject target, ref float __result)
+        {
+            if (target.GetComponent<CyclopsDecoyManager>() != null && MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, Module.Module.CyclopsHullReinforcement.thisTechType))
+            {
+                __result /= 2;   
+            }
+        }
+    }
 }
