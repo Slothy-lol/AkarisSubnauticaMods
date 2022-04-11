6cc72763bc6e5c74b79d72ba3cc1dc8ca56a64f8
Slothy-lol <snorlaxlover10@gmail.com>
2022-04-11 06:48:25 +0100

    Update Patches.cs

--- CyclopsHullReinforcement/Patches.cs
+++ CyclopsHullReinforcement/Patches.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HarmonyLib;
 
 namespace CyclopsHullReinforcement.Patches
 {
-    public class Patches
-    {
 
-    }
 }
