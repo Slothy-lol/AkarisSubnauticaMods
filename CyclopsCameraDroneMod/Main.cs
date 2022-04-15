594e77a8419084f02c1827b1ef23d7666a31cc0f
Slothy-lol <snorlaxlover10@gmail.com>
2022-04-15 11:08:10 +0100

    i fixed my mistake

--- CyclopsCameraDroneMod/Main.cs
+++ CyclopsCameraDroneMod/Main.cs
@@ -523,10 +523,6 @@ namespace CyclopsCameraDroneMod.Main
         public static void ScanFunctionality(MapRoomCamera mapRoomCamera)
         {
             if (!Targeting.GetTarget(mapRoomCamera.gameObject, 20, out var gameObject1, out float distance)) return;
-
-        public static void ScanFunctionality(MapRoomCamera mapRoomCamera)
-        {
-            if (!Targeting.GetTarget(mapRoomCamera.gameObject, 20, out var gameObject1, out float distance)) return;
             PrefabIdentifier Identifier = gameObject1.GetComponentInParent<PrefabIdentifier>();
             if (Identifier == null) return;
             GameObject gameObject4 = Identifier.gameObject;
