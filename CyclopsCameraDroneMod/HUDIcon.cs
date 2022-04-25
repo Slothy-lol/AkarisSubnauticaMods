90a7fa774dce02cb62a0798af3217aaaed25e560
Slothy-lol <snorlaxlover10@gmail.com>
2022-04-25 15:16:34 +0100

    HUD icon shit

--- CyclopsCameraDroneMod/HUDIcon.cs
+++ CyclopsCameraDroneMod/HUDIcon.cs
@@ -1,11 +1,11 @@
 ﻿using MoreCyclopsUpgrades.API;
 using MoreCyclopsUpgrades.API.StatusIcons;
-using QModManager.API.ModLoading;
 using System.Reflection;
 using Sprite = Atlas.Sprite;
 using UnityEngine;
 using SMLHelper.V2.Utility;
 using System.IO;
+using CyclopsCameraDroneMod.Modules;
 
 namespace CyclopsCameraDroneMod.HUDIcon
 {
@@ -13,14 +13,28 @@ namespace CyclopsCameraDroneMod.HUDIcon
     {
         public CyclopsCameraDroneHUDIcon(SubRoot cyclops) : base(cyclops)
         {
-
+            // We're no strangers to love
+            // You know the rules
+            // And so do I
         }
 
-        public override bool ShowStatusIcon => (MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, Modules.CyclopsCameraDroneIndustry.thisTechType) || MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, Modules.CyclopsCameraDroneExploration.thisTechType) || MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, Modules.CyclopsCameraDroneUltimate.thisTechType));
+        public override bool ShowStatusIcon => 
+            (MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, CyclopsCameraDroneIndustry.thisTechType) 
+            || MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, CyclopsCameraDroneExploration.thisTechType) 
+            || MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, CyclopsCameraDroneUltimate.thisTechType));
+
         readonly string AssetsFolder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets");
 
         public override Sprite StatusSprite()
         {
+            if (MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, CyclopsCameraDroneUltimate.thisTechType))
+            {
+                return ImageUtils.LoadSpriteFromFile(Path.Combine(AssetsFolder, "CameraDroneModule2.png"));
+            }
+            else if (MCUServices.CrossMod.HasUpgradeInstalled(Player.main.currentSub, CyclopsCameraDroneIndustry.thisTechType))
+            {
+                return ImageUtils.LoadSpriteFromFile(Path.Combine(AssetsFolder, "CameraDroneModule1.png"));
+            }
             return ImageUtils.LoadSpriteFromFile(Path.Combine(AssetsFolder, "CameraDroneModule0.png"));
         }
 
@@ -28,10 +42,9 @@ namespace CyclopsCameraDroneMod.HUDIcon
         {
             if (Time.time < Main.Main.timeNextUseDrone)
             {
-                return $"Time until next use: {Main.Main.timeNextUseDrone - Time.time}.";
+                return $"Time until next use: {(int)(Main.Main.timeNextUseDrone - Time.time)}.";
             }
-            else
-                return "Drone Ready.";
+            else { return "Drone Ready."; }
         }
 
         public override Color StatusTextColor()
@@ -40,8 +53,7 @@ namespace CyclopsCameraDroneMod.HUDIcon
             {
                 return Color.red;
             }
-            else
-                return Color.white;
+            else { return Color.white; }
         }
     }
 }
